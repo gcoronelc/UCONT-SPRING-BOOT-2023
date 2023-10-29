b908bf44b0e7bed77b0e5fe4cc2b97d24dacd3b5
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2023-10-29 11:56:23 -0500

    Semana 2

--- Semana02/demo03/src/main/java/pe/edu/ucontinental/app/controller/AppController2.java
+++ Semana02/demo03/src/main/java/pe/edu/ucontinental/app/controller/AppController2.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/app2")
 public class AppController2 {
+	
+	@GetMapping
+	public String gustavo() {
+		return "gustavo";
+	}
 
 	@GetMapping("/index")
 	public String home() {
