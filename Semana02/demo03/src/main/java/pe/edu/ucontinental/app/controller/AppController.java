8f71785ef647c6ac9633f64169f5a52f96ffca71
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2023-10-29 11:49:27 -0500

    Semana 2

--- Semana02/demo03/src/main/java/pe/edu/ucontinental/app/controller/AppController.java
+++ Semana02/demo03/src/main/java/pe/edu/ucontinental/app/controller/AppController.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/app1")
 public class AppController {
 
 	@GetMapping({ "", "/", "/index", "/home", "/gustavo" })
---
+++ Semana02/demo03/src/main/java/pe/edu/ucontinental/app/controller/AppController2.java
@@ -0,0 +1,17 @@
+package pe.edu.ucontinental.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app2")
+public class AppController2 {
+
+	@GetMapping("/index")
+	public String home() {
+		return "promedio";		
+	}
+	
+	
+}
